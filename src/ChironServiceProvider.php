c380b12a139b90f137daf61decb8b7421e74bc9a
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-10-03 00:59:05 +0700

    Initial commit

---
+++ src/ChironServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gaia\Chiron;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChironServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(resource_path('views/vendor/chiron'), 'chiron');
+        $this->loadViewsFrom(realpath(__DIR__ . '/resources/views'), 'chiron');
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/chiron'),
+            __DIR__ . '/config' => config_path()
+        ]);
+    }
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
