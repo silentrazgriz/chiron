b0e47432f2bdb9a4822a5be7422474ea082a8df5
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-10-20 16:43:29 +0700

    add /edit on update action

--- src/resources/views/components/buttons/update.blade.php
+++ src/resources/views/components/buttons/update.blade.php
@@ -1,4 +1,4 @@
 @if($options['actions']['update'])
-    <a href="{{ (null == $options['route']['update'] ? route($options['route']['default'] . '.edit', $collection['id']) : $options['route']['update'] . '/' . $collection['id']) }}"
+    <a href="{{ (null == $options['route']['update'] ? route($options['route']['default'] . '.edit', $collection['id']) : $options['route']['update'] . '/' . $collection['id'] . '/edit') }}"
        class="btn btn-sm btn-info"><i class="fas fa-edit"></i> Edit</a>
 @endif
