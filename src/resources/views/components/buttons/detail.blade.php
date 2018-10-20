c6a2f3335da2329b5272d73ee9fc7f6ce9c35184
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-10-20 16:35:54 +0700

    fix url action

--- src/resources/views/components/buttons/detail.blade.php
+++ src/resources/views/components/buttons/detail.blade.php
@@ -1,5 +1,5 @@
 @if($options['actions']['detail'])
-    <a href="{{ (null == $options['route']['show'] ? route($options['route']['default'] . '.show', $collection['id']) : $options['route']['show'] . $collection['id']) }}"
+    <a href="{{ (null == $options['route']['show'] ? route($options['route']['default'] . '.show', $collection['id']) : $options['route']['show'] . '/' . $collection['id']) }}"
        class="btn btn-sm btn-facebook">
         <i class="fas fa-info"></i> Detail
     </a>
